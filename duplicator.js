a8a7fb53e21e386bbb73dbcaad99fbc67805ef1f
lunev <lunevdev@gmail.com>
2017-10-19 16:23:06 +0300

    Deleted trash

--- duplicator.js
+++
@@ -1,8 +0,0 @@
-// content.js
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      window.open(document.location.href + 'admin', '_blank', 'toolbar=yes, location=yes, status=yes, menubar=yes, scrollbars=yes');
-    }
-  }
-);
